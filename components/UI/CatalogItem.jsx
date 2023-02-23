c86b04a05edf28e99dd936f4c05cd856bc0077bd
maskharor.amnan05@gmail.com <maskharor.amnan05@gmail.com>
2023-02-23 09:45:50 +0700

    delete launch button

--- components/UI/CatalogItem.jsx
+++ components/UI/CatalogItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import classes from "../../styles/catalog-item.module.css";
 import Image from "next/image";
-import Link from "next/link";
 
 const CatalogItem = (props) => {
   const { title, img, liveUrl, keyword } = props.item;
@@ -18,12 +17,6 @@ const CatalogItem = (props) => {
 
       <div className={`${classes.catalog_img}`}>
         <Image alt="portfolio-img" src={img} width="280" height="280" />
-      </div>
-
-      <div className={`${classes.catalog_live} bg-transparent`}>
-        <button className="primary_btn">
-          <Link href={liveUrl}>Launch</Link>
-        </button>
       </div>
     </div>
   );
